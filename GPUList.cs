8200cfb449724c92ca5fc02080b1987ce1644ab5
Naka Nobuyuki <nobu.nakata@gmail.com>
2017-04-07 11:11:01 +0900

    Array & List を整理統合

--- GPUList.cs
+++ GPUList.cs
@@ -3,96 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime.InteropServices;
 
-public class GPUList<S> : System.IDisposable where S : struct {
-    public const int MINIMUM_CAPACITY = 16;
-    int count;
-    S[] list;
-    bool bufferIsDirty;
-    ComputeBuffer buf;
+namespace BasicGPUParticle {
 
-    public GPUList(int initialCapasity = MINIMUM_CAPACITY) {
-        count = 0;
-        Resize (OptimalCapacity (initialCapasity));
-    }
+	public class GPUList<T> : GPUArray<T> where T : struct {
+	    public const int MINIMUM_CAPACITY = 16;
 
-    #region List
-    public int Count { get { return count; } }
-    public S[] CPUBuffer { get { return list; } }
-    public ComputeBuffer GPUBuffer { 
-        get {
-            if (bufferIsDirty) {
-                bufferIsDirty = false;
-                buf.SetData (list);
-            }
-            return buf; 
-        }
-    }
-	public void Replace(S[] srcList) {
-		bufferIsDirty = true;
-		Resize (srcList.Length);
-		System.Array.Copy (srcList, list, srcList.Length);
+		public GPUList(int initialCapacity = MINIMUM_CAPACITY) : base(initialCapacity) {
+	        Resize (OptimalCapacity (initialCapacity));
+	    }
+
+		protected override bool TryChangeCount (int nextCount) {
+			Resize (OptimalCapacity (nextCount));
+			return base.TryChangeCount (nextCount);
+		}
+	    protected virtual int OptimalCapacity(int count) {
+	        return SmallestPowerOfTwoGreaterThan(Mathf.Max(MINIMUM_CAPACITY, count));
+	    }
+	    protected virtual void Resize (int targetCapacity) {
+			if (cpuBuffer == null || cpuBuffer.Length != targetCapacity) {
+				System.Array.Resize (ref cpuBuffer, targetCapacity);
+				Release (ref gpuBuffer);
+				gpuBuffer = CreateGPUBuffer (targetCapacity);
+				Upload ();
+				bufferIsDirty = false;
+	        }
+	    }
+	    public static int SmallestPowerOfTwoGreaterThan (int n) {
+	        --n;
+	        n |= n >> 1;
+	        n |= n >> 2;
+	        n |= n >> 4;
+	        n |= n >> 8;
+	        n |= n >> 16;
+	        return ++n;
+	    }
 	}
-    #endregion
-
-    #region Elements 
-    public S this[int i] {
-        get { return list [i]; }
-        set { 
-            bufferIsDirty = true;
-            list [i] = value; 
-        }
-    }
-    public void Push(S s) {
-        bufferIsDirty = true;
-        Resize (OptimalCapacity (count + 1));
-        list [count++] = s;
-    }
-	public S Pop() {
-		bufferIsDirty = true;
-		var s = list [--count];
-		Resize (OptimalCapacity (count));
-		return s;
-	}
-    public S RemoveAt(int indexOf) {
-        bufferIsDirty = true;
-
-        var s = list [indexOf];
-        System.Array.Copy (list, indexOf + 1, list, indexOf, count - 1 - indexOf);
-        Resize (OptimalCapacity (--count));
-        return s;
-    }
-    #endregion
-
-    int OptimalCapacity(int count) {
-        return SmallestPowerOfTwoGreaterThan(Mathf.Max(MINIMUM_CAPACITY, count));
-    }
-    void Resize (int targetCapacity) {
-        if (list == null || list.Length != targetCapacity) {
-            System.Array.Resize (ref list, targetCapacity);
-            Release (ref buf);
-            buf = new ComputeBuffer (targetCapacity, Marshal.SizeOf (typeof(S)));
-        }
-    }
-
-    static void Release<T>(ref T buf) where T : class, System.IDisposable  {
-        if (buf != null) {
-            buf.Dispose ();
-            buf = null;
-        }
-    }
-    static int SmallestPowerOfTwoGreaterThan (int n) {
-        --n;
-        n |= n >> 1;
-        n |= n >> 2;
-        n |= n >> 4;
-        n |= n >> 8;
-        n |= n >> 16;
-        return ++n;
-    }
-
-    #region IDisposable implementation
-    public void Dispose () {
-        Release (ref buf);
-    }
-    #endregion
 }
